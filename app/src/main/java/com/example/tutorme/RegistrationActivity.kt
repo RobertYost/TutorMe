089e6de32b782ed14622768e969e316eaa0e8b82
Bobby Yost <bobby.9202@gmail.com>
2019-10-03 17:18:49 -0400

    Add rest of fields to registration activity, and initialize their references in activity code

--- app/src/main/java/com/example/tutorme/RegistrationActivity.kt
+++ app/src/main/java/com/example/tutorme/RegistrationActivity.kt
@@ -1,12 +1,36 @@
 package com.example.tutorme
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class RegistrationActivity : AppCompatActivity() {
 
+    private lateinit var firstNameTxt: EditText
+    private lateinit var lastNameTxt: EditText
+    private lateinit var emailTxt: EditText
+    private lateinit var passwordTxt: EditText
+    private lateinit var passwordConfirmationTxt: EditText
+    private lateinit var schoolSpinner: Spinner
+    private lateinit var submitBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        firstNameTxt = findViewById(R.id.firstNameTxt)
+        lastNameTxt = findViewById(R.id.lastNameTxt)
+        emailTxt = findViewById(R.id.emailTxt)
+        passwordTxt = findViewById(R.id.passwordTxt)
+        passwordConfirmationTxt = findViewById(R.id.confirmPasswordTxt)
+        schoolSpinner = findViewById(R.id.schoolSpinner)
+        submitBtn = findViewById(R.id.submitBtn)
+
+        submitBtn.setOnClickListener { view: View ->
+            // TODO: Create account, navigate to swipeActivity
+        }
     }
 }
