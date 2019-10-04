81bf25bf5fb265dc87b2db2ac05472bb53416424
VanFleet0351 <k.vanfleet.7@gmail.com>
2019-10-03 23:15:42 -0400

    added facebook logo to button, and added clickable text to sign in below buttons. Listeners are set up and waiting for logic.

--- app/src/main/java/com/example/tutorme/MainActivity.kt
+++ app/src/main/java/com/example/tutorme/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.tutorme
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var createAccountButton: Button
-    private lateinit var facebookLogInButton: Button
+    private lateinit var facebookLogInButton: ImageButton
+
+    private lateinit var signInTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         createAccountButton = findViewById(R.id.create_account_button)
         facebookLogInButton = findViewById(R.id.facebook_log_in_button)
+        signInTextView = findViewById(R.id.sign_in_text_view)
 
         createAccountButton.setOnClickListener { view: View ->
             //TODO: go to account creation page
         }
         facebookLogInButton.setOnClickListener { view: View ->
             //TODO: log in with facebook
+        }
+        signInTextView.setOnClickListener {view: View ->
+            //TODO: go to sign in page
         }
     }
 
