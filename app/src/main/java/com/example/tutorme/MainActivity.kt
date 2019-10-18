1e1ddfb8849d55228154e2664e1f0401466cebea
Bobby <bobby.9202@gmail.com>
2019-10-17 21:52:46 -0400

    add firebase auth UI library, replace homegrown login activity with firebase's pre-built solution

--- app/src/main/java/com/example/tutorme/MainActivity.kt
+++ app/src/main/java/com/example/tutorme/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.tutorme
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
-import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 
 private const val TAG = "MainActivity"
@@ -18,34 +22,72 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var signInTextView: TextView
 
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_view)
+        createSignInIntent()
 
         Log.d(TAG, "onCreate()")
 
-        createAccountButton = findViewById(R.id.create_account_button)
-        facebookLogInButton = findViewById(R.id.facebook_log_in_button)
-        signInTextView = findViewById(R.id.sign_in_text_view)
+//        createAccountButton = findViewById(R.id.create_account_button)
+//        facebookLogInButton = findViewById(R.id.facebook_log_in_button)
+//        signInTextView = findViewById(R.id.sign_in_text_view)
+//
+//        createAccountButton.setOnClickListener {
+//            val intent = Intent(this, RegistrationActivity::class.java)
+//            startActivity(intent)
+//        }
+//        facebookLogInButton.setOnClickListener {
+//            //TODO: log in with facebook
+//        }
+//        signInTextView.setOnClickListener {
+//            //TODO: go to sign in page
+//            //links to settings for testing
+//            val intent = Intent(this, SettingsActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
 
-        createAccountButton.setOnClickListener {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            startActivity(intent)
-        }
-        facebookLogInButton.setOnClickListener {
-            //TODO: log in with facebook
-        }
-        signInTextView.setOnClickListener {
-            //TODO: go to sign in page
-            //links to settings for testing
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // Navigate to new activity
+//                finish()
+            } else {
+                // Sign in failed. If response is null the user canceled the sign-in flow using
+                // the back button. Otherwise check response.getError().getErrorCode() and handle
+                // the error
+                Log.d(TAG, "Failed to sign in")
+            }
         }
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG,"onStart()")
+        Log.d(TAG, "onStart()")
     }
 
     override fun onResume() {
@@ -66,6 +108,10 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy()")
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 
 }
