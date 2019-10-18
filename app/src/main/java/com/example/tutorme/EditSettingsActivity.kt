3803a88d38c6dca1425693fdb99e66e9220a5255
Bobby <bobby.9202@gmail.com>
2019-10-17 21:57:29 -0400

    replace findViewById usage in EditSettingsActivity with view binding

--- app/src/main/java/com/example/tutorme/EditSettingsActivity.kt
+++ app/src/main/java/com/example/tutorme/EditSettingsActivity.kt
@@ -1,37 +1,21 @@
 package com.example.tutorme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tutorme.databinding.ActivityEditSettingsBinding
 
 class EditSettingsActivity : AppCompatActivity() {
 
-    private lateinit var saveButton: Button
-    private lateinit var firstNameEditText: EditText
-    private lateinit var lastNameEditText: EditText
-    private lateinit var emailEditText: EditText
-    private lateinit var passwordEditText: EditText
-    private lateinit var schoolEditText: EditText
-    private lateinit var currencyEditText: EditText
-    private lateinit var profilePicUrlEditText: EditText
-
-
+    // Using view-binding from arch-components (requires Android Studio 3.6 Canary 11+)
+    private lateinit var binding: ActivityEditSettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_settings)
+        binding = ActivityEditSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        saveButton = findViewById(R.id.edit_settings_save_button)
-        firstNameEditText = findViewById(R.id.edit_settings_first_name)
-        lastNameEditText = findViewById(R.id.edit_settings_last_name)
-        emailEditText = findViewById(R.id.edit_settings_email)
-        passwordEditText = findViewById(R.id.edit_settings_password)
-        schoolEditText = findViewById(R.id.edit_settings_school)
-        currencyEditText = findViewById(R.id.edit_settings_currency)
-        profilePicUrlEditText = findViewById(R.id.edit_settings_profile_pic)
 
-        saveButton.setOnClickListener {
+        binding.editSettingsSaveButton.setOnClickListener {
             //TODO: save entered settings
         }
     }
--- app/src/main/java/com/example/tutorme/MainActivity.kt
+++ app/src/main/java/com/example/tutorme/MainActivity.kt
@@ -4,9 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
@@ -16,12 +13,6 @@ import com.google.firebase.auth.FirebaseAuth
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var createAccountButton: Button
-    private lateinit var facebookLogInButton: ImageButton
-
-    private lateinit var signInTextView: TextView
-
 
     private fun createSignInIntent() {
         val providers = arrayListOf(
@@ -45,24 +36,6 @@ class MainActivity : AppCompatActivity() {
         createSignInIntent()
 
         Log.d(TAG, "onCreate()")
-
-//        createAccountButton = findViewById(R.id.create_account_button)
-//        facebookLogInButton = findViewById(R.id.facebook_log_in_button)
-//        signInTextView = findViewById(R.id.sign_in_text_view)
-//
-//        createAccountButton.setOnClickListener {
-//            val intent = Intent(this, RegistrationActivity::class.java)
-//            startActivity(intent)
-//        }
-//        facebookLogInButton.setOnClickListener {
-//            //TODO: log in with facebook
-//        }
-//        signInTextView.setOnClickListener {
-//            //TODO: go to sign in page
-//            //links to settings for testing
-//            val intent = Intent(this, SettingsActivity::class.java)
-//            startActivity(intent)
-//        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -75,7 +48,6 @@ class MainActivity : AppCompatActivity() {
                 // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser
                 // Navigate to new activity
-//                finish()
             } else {
                 // Sign in failed. If response is null the user canceled the sign-in flow using
                 // the back button. Otherwise check response.getError().getErrorCode() and handle
