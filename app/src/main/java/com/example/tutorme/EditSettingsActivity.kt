204da87f1b5e9440588f8235a23c0ff878dd43c0
VanFleet0351 <k.vanfleet.7@gmail.com>
2019-11-07 16:05:51 -0500

    Added Photo selection in edit settings but it currently doesnt save. Also updated layout for edit settings

--- app/src/main/java/com/example/tutorme/EditSettingsActivity.kt
+++ app/src/main/java/com/example/tutorme/EditSettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.tutorme
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +14,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.example.tutorme.R
 import com.example.tutorme.swipe_view.SwipeActivity
+import kotlinx.android.synthetic.main.activity_edit_settings.*
 
+private const val TAG = "editsettings"
 class EditSettingsActivity : AppCompatActivity() {
 
     // Using view-binding from arch-components (requires Android Studio 3.6 Canary 11+)
@@ -52,6 +57,12 @@ class EditSettingsActivity : AppCompatActivity() {
             }
         }
 
+        binding.selectPhotoEditSettings.setOnClickListener {
+            Log.d(TAG, "try to select photo")
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
         binding.editSchoolBtn.setOnClickListener {
                 // Redirects back to the tutor list page after saving
                 val intent = Intent(this, SchoolListActivity::class.java)
@@ -85,6 +96,7 @@ class EditSettingsActivity : AppCompatActivity() {
                 val intent = Intent(this, SwipeActivity::class.java)
                 startActivity(intent)
             }
+
             //TODO: Update vs. Create (Currently works fine as is, maybe change for NFR Checkpoint)
 //            if(!userExists){
 //                Log.d("DEBUG", "Should have created user")
@@ -93,9 +105,19 @@ class EditSettingsActivity : AppCompatActivity() {
 //                db.collection("students").document(FirebaseAuth.getInstance().currentUser!!.uid).set(settings, SetOptions.merge())
 //            }
 
+
         }
     }
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            val uri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            profilePicImgView.setBackgroundDrawable(bitmapDrawable)
+            select_photo_edit_settings.alpha = 0f
+        }
+    }
     override fun onResume() {
         val thisIntent = intent
         if (thisIntent.getStringExtra("school") != null) {
