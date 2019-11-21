575e61293146a000e322404086223ba250dd6e47
VanFleet0351 <k.vanfleet.7@gmail.com>
2019-11-20 19:45:44 -0500

    added view model to settings to reduce network load

--- app/src/main/java/com/example/tutorme/SettingsActivity.kt
+++ app/src/main/java/com/example/tutorme/SettingsActivity.kt
@@ -1,22 +1,16 @@
 package com.example.tutorme
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.Matrix
-import android.media.ExifInterface
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.example.tutorme.databinding.ActivitySettingsBinding
 import com.example.tutorme.models.Student
 import com.example.tutorme.swipe_view.SwipeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
@@ -24,7 +18,7 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
 
     companion object{
-        const val DEFUALT_PROFILE_PICTURE = "https://firebasestorage.googleapis.com/v0/b/tutorme-" +
+        const val DEFAULT_PROFILE_PICTURE = "https://firebasestorage.googleapis.com/v0/b/tutorme-" +
                 "backend.appspot.com/o/images%2F44612a04-e07f-408b-a377-572670c82a33?alt=media&token" +
                 "=8515b3e0-d428-406e-a0dd-8324ae493c0c"
     }
@@ -34,27 +28,34 @@ class SettingsActivity : AppCompatActivity() {
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val db = FirebaseFirestore.getInstance()
-
-        val student =
-            db.collection("students").document(FirebaseAuth.getInstance().currentUser!!.uid)
-        var oldSettings: Student?
-        student.get().addOnSuccessListener { documentSnapshot ->
-            oldSettings = documentSnapshot.toObject(Student::class.java)
-
-            binding.settingsEmail.text = FirebaseAuth.getInstance().currentUser?.email
-            binding.settingsFirstName.text = oldSettings?.first_name
-            binding.settingsLastName.text = oldSettings?.last_name
-            binding.settingsProfilePictureUrl.text = oldSettings?.profile_picture_url
-            binding.settingsSchool.text = oldSettings?.school
-
-            if(oldSettings?.profile_picture_url == null || oldSettings?.profile_picture_url!!.isEmpty()){
-                Glide.with(this).load(DEFUALT_PROFILE_PICTURE).into(profile_imageview_settings)
-            }else{
-                Glide.with(this).load(oldSettings?.profile_picture_url).into(profile_imageview_settings)
+        val settingsViewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        val user = settingsViewModel.currentUser
+        if (user == null) {
+            val databaseUserEntry = FirebaseFirestore.getInstance().collection("students").
+                document(FirebaseAuth.getInstance().currentUser!!.uid)
+            databaseUserEntry.get().addOnSuccessListener {
+                settingsViewModel.currentUser = it.toObject(Student::class.java)!!
+                loadTextAndPicture(it.toObject(Student::class.java)!!)
             }
-
+        } else {
+            loadTextAndPicture(user)
         }
+
+
+//        val db = FirebaseFirestore.getInstance()
+//        val student =
+//            db.collection("students").document(FirebaseAuth.getInstance().currentUser!!.uid)
+//        var oldSettings: Student?
+//        student.get().addOnSuccessListener { documentSnapshot ->
+//            oldSettings = documentSnapshot.toObject(Student::class.java)
+//
+//            binding.settingsEmail.text = FirebaseAuth.getInstance().currentUser?.email
+//            binding.settingsFirstName.text = oldSettings?.first_name
+//            binding.settingsLastName.text = oldSettings?.last_name
+//            binding.settingsProfilePictureUrl.text = oldSettings?.profile_picture_url
+//            binding.settingsSchool.text = oldSettings?.school
+//        }
+
 
 
         binding.settingsEditButton.setOnClickListener{
@@ -63,12 +64,28 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         binding.deleteAccButton.setOnClickListener{
-            db.collection("students").document(FirebaseAuth.getInstance().currentUser!!.uid)
+            FirebaseFirestore.getInstance()
+                .collection("students")
+                .document(FirebaseAuth.getInstance().currentUser!!.uid)
                 .delete()
                 .addOnSuccessListener { Log.d("DELETE", "DocumentSnapshot successfully deleted!") }
                 .addOnFailureListener { e -> Log.w("DEBUG", "Error deleting document", e) }
             val intent = Intent(this, SwipeActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun loadTextAndPicture(user: Student){
+        binding.settingsEmail.text = user.email
+        binding.settingsFirstName.text = user.first_name
+        binding.settingsLastName.text = user.last_name
+        binding.settingsProfilePictureUrl.text = user.profile_picture_url
+        binding.settingsSchool.text = user.school
+
+        if(user?.profile_picture_url == null || user.profile_picture_url!!.isEmpty()){
+            Glide.with(this).load(DEFAULT_PROFILE_PICTURE).into(profile_imageview_settings)
+        }else{
+            Glide.with(this).load(user.profile_picture_url).into(profile_imageview_settings)
         }
     }
 }
